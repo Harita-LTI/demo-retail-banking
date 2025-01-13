1ed078ae5560acb5aaa52ed1a3ad951d611c925c
ankitsen-ltim <ankit.sen@ltimindtree.com>
2025-01-13 13:57:18 +0530

    Fix css for home screen nav bar 1.

--- src/components/common/Navbar1/index.tsx
+++ src/components/common/Navbar1/index.tsx
@@ -17,16 +17,15 @@ function NavBar1() {
   return(
     <>
       <div className="main">
-        <div className="child">
+        <div className="child" style={{justifyContent: "start"}} >
           <div className="option right-separator">Personal</div>
-          <div className="option right-separator">Retail Business Banking</div>
           <div className="option right-separator">Business</div>
           <div className="option right-separator">Asset Management</div>
-          <div className="option right-separator">Global Private Banking</div>
-          <div className="option right-separator">HSBC Securities & Capital Markets</div>
-          <div className="option">HSBC GIFT City</div>
+          <div className="option right-separator">Private Banking</div>
+          <div className="option right-separator">Securities & Capital Markets</div>
+          <div className="option">GIFT City</div>
         </div>
-        <div className="child">
+        <div className="child" style={{justifyContent: "end"}}>
           <div className="option">English</div>
           <div className="option">
             <FontAwesomeIcon icon={faSearch} />
